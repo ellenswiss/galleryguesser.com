e19a9f889a17ac933336c7d9f0e445167cea8f09
ellenswiss <ellen@seesawmap.com>
2025-06-12 21:05:08 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 const exhibitions = [
   {
-    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/_wSN2htOSAIAMiDG0qKbgg",
-    headline: "1001 Nights [+1]",
-    artist: "Rachel Harrison",
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https://seesaw-photos.s3.amazonaws.com/_wSN2htOSAIAMiDG0qKbgg",
+    headline: "Robert Janitz: 1001 Nights [+1]",
+    artist: "Robert Janitz",
     gallery: "Canada",
     city: "New York",
     options: [
-      "1001 Nights [+1]",
+      "Robert Janitz: 1001 Nights [+1]",
       "Remote Work",
       "Rachel Harrison: Perimeter",
       "Soft Landing"
