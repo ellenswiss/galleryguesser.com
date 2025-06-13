91a86a586196d29ae58c137d327e68c1edc40cec
ellenswiss <ellen@seesawmap.com>
2025-06-13 14:24:54 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,15 +1,15 @@
 const exhibitions = [
   {
     image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/_wSN2htOSAIAMiDG0qKbgg",
-    headline: "Robert Janitz: 1001 Nights [+1]",
+    headline: "Robert Janitz at Canada",
     artist: "Robert Janitz",
     gallery: "Canada",
     city: "New York",
     options: [
-      "Robert Janitz: 1001 Nights [+1]",
-      "Sam Moyer: Woman with Holes",
-      "Rachel Harrison: The Friedmann Equations",
-      "Willem de Kooning: Endless Painting"
+      "Robert Janitz at Canada",
+      "Sam Moyer at Hill Art Foundation",
+      "Rachel Harrison at Greene Naftali",
+      "Willem de Kooning at Gagosian"
     ]
   },
   {
