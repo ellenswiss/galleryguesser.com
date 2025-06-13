4305595c70c440afe67d0f16409e3074ee4e2244
ellenswiss <ellen@seesawmap.com>
2025-06-13 14:27:56 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const exhibitions = [
   {
     image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/_wSN2htOSAIAMiDG0qKbgg",
-    headline: "Robert Janitz at Canada",
+    headline: "Robert Janitz: 1001 Nights [+1]",
     artist: "Robert Janitz",
     gallery: "Canada",
     city: "New York",
