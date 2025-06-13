dcc00b423080f620cdce4e83fa730dac33bd8ab0
ellenswiss <ellen@seesawmap.com>
2025-06-13 13:22:25 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const exhibitions = [
   {
-    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https://seesaw-photos.s3.amazonaws.com/_wSN2htOSAIAMiDG0qKbgg",
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/_wSN2htOSAIAMiDG0qKbgg",
     headline: "Robert Janitz: 1001 Nights [+1]",
     artist: "Robert Janitz",
     gallery: "Canada",
@@ -10,6 +10,58 @@ const exhibitions = [
       "Sam Moyer: Woman with Holes",
       "Rachel Harrison: The Friedmann Equations",
       "Willem de Kooning: Endless Painting"
+    ]
+  },
+  {
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/pzK5QcMf95igbRojcDCI8w",
+    headline: "Willem de Kooning: Endless Painting",
+    artist: "Willem de Kooning",
+    gallery: "Gagosian",
+    city: "New York",
+    options: [
+      "Willem de Kooning: Endless Painting",
+      "Charline von Heyl: Snake Eyes",
+      "Julie Mehretu: Conjured Parts",
+      "Robert Janitz: 1001 Nights [+1]"
+    ]
+  },
+  {
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/rA0qG3Sh3CVhwWVsXRzoVQ",
+    headline: "Sam Moyer: Woman with Holes",
+    artist: "Sam Moyer",
+    gallery: "Hill Art Foundation",
+    city: "New York",
+    options: [
+      "Sam Moyer: Woman with Holes",
+      "Rachel Rose: Enclosure",
+      "Willem de Kooning: Endless Painting",
+      "Tschabalala Self: Cotton Mouth"
+    ]
+  },
+  {
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/ZNnv5YH-ytC_yTYxddQhfg",
+    headline: "Mary Weatherford: The Surrealist",
+    artist: "Mary Weatherford",
+    gallery: "David Kordansky",
+    city: "Los Angeles",
+    options: [
+      "Mary Weatherford: The Surrealist",
+      "Amy Sillman: After Metamorphoses",
+      "Laura Owens: Untitled",
+      "Sam Moyer: Woman with Holes"
+    ]
+  },
+  {
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/35C9vbwlkyHzzJatqfGkOA",
+    headline: "Ugo Rondinone: The Rainbow Body",
+    artist: "Ugo Rondinone",
+    gallery: "Sadie Coles HQ",
+    city: "London",
+    options: [
+      "Ugo Rondinone: The Rainbow Body",
+      "Wade Guyton: For Us",
+      "Mary Weatherford: The Surrealist",
+      "Roni Horn: Remembered Words"
     ]
   }
 ];
