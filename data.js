c29aac431da1470e458585fd237e50400a09fa4c
ellenswiss <ellen@seesawmap.com>
2025-06-12 20:58:30 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const exhibitions = [
+  {
+    image: "https://seesawmap.com/admin/photos/185651",
+    headline: "Robert Janitz: 1001 Nights [ +1 ]",
+    options: [
+      "1001 Nights [ +1 ]",
+      "Doron Langberg: Tender",
+      "Amy Sillman: After Metamorphoses",
+      "Rachel Harrison: Life Hack"
+    ]
+  }
+];
