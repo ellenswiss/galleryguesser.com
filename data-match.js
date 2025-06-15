e231aacd832f42c96226c3dd6cdb0dc04216aeb8
ellenswiss <ellen@seesawmap.com>
2025-06-15 15:41:24 -0400

    Update data-match.js

--- data-match.js
+++ data-match.js
@@ -1,27 +1,27 @@
-const matchData = [
+const exhibitions = [
   {
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/_wSN2htOSAIAMiDG0qKbgg",
     artist: "Robert Janitz",
-    gallery: "Canada",
-    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/_wSN2htOSAIAMiDG0qKbgg"
+    gallery: "Canada"
   },
   {
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/pzK5QcMf95igbRojcDCI8w",
     artist: "Willem de Kooning",
-    gallery: "Gagosian",
-    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/pzK5QcMf95igbRojcDCI8w"
+    gallery: "Gagosian"
   },
   {
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/rA0qG3Sh3CVhwWVsXRzoVQ",
     artist: "Sam Moyer",
-    gallery: "Hill Art Foundation",
-    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/rA0qG3Sh3CVhwWVsXRzoVQ"
+    gallery: "Hill Art Foundation"
   },
   {
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/ZNnv5YH-ytC_yTYxddQhfg",
     artist: "Mary Weatherford",
-    gallery: "David Kordansky",
-    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/ZNnv5YH-ytC_yTYxddQhfg"
+    gallery: "David Kordansky"
   },
   {
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/35C9vbwlkyHzzJatqfGkOA",
     artist: "Ugo Rondinone",
-    gallery: "Sadie Coles HQ",
-    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/35C9vbwlkyHzzJatqfGkOA"
+    gallery: "Sadie Coles HQ"
   }
 ];
