80aec614ad0c4cad0fbca7757e105e473539dfc4
ellenswiss <ellen@seesawmap.com>
2025-06-15 15:14:29 -0400

    Update data-match.js

--- data-match.js
+++ data-match.js
@@ -1,1 +1,18 @@
+const exhibitions = [
+  {
+    image: "image1.jpg",
+    artists: ["Rachel Harrison", "Simone Leigh", "Mark Leckey", "Cecily Brown"],
+    venues: ["Greene Naftali", "Hauser & Wirth", "Gagosian", "David Zwirner"],
+    correctArtist: "Rachel Harrison",
+    correctVenue: "Greene Naftali"
+  },
+  {
+    image: "image2.jpg",
+    artists: ["Toyin Ojih Odutola", "Julie Mehretu", "Jadé Fadojutimi", "Amy Sillman"],
+    venues: ["Marian Goodman", "Pace", "Gladstone", "White Cube"],
+    correctArtist: "Jadé Fadojutimi",
+    correctVenue: "Gladstone"
+  }
+  // Add more!
+];
 
