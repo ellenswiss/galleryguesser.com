ec3573b7ad447f376004c30124a70b68948fce53
ellenswiss <ellen@seesawmap.com>
2025-06-15 15:28:31 -0400

    Update data-match.js

--- data-match.js
+++ data-match.js
@@ -1,18 +1,27 @@
-const exhibitions = [
+const matchData = [
   {
-    image: "image1.jpg",
-    artists: ["Rachel Harrison", "Simone Leigh", "Mark Leckey", "Cecily Brown"],
-    venues: ["Greene Naftali", "Hauser & Wirth", "Gagosian", "David Zwirner"],
-    correctArtist: "Rachel Harrison",
-    correctVenue: "Greene Naftali"
+    artist: "Robert Janitz",
+    gallery: "Canada",
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/_wSN2htOSAIAMiDG0qKbgg"
   },
   {
-    image: "image2.jpg",
-    artists: ["Toyin Ojih Odutola", "Julie Mehretu", "Jadé Fadojutimi", "Amy Sillman"],
-    venues: ["Marian Goodman", "Pace", "Gladstone", "White Cube"],
-    correctArtist: "Jadé Fadojutimi",
-    correctVenue: "Gladstone"
+    artist: "Willem de Kooning",
+    gallery: "Gagosian",
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/pzK5QcMf95igbRojcDCI8w"
+  },
+  {
+    artist: "Sam Moyer",
+    gallery: "Hill Art Foundation",
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/rA0qG3Sh3CVhwWVsXRzoVQ"
+  },
+  {
+    artist: "Mary Weatherford",
+    gallery: "David Kordansky",
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/ZNnv5YH-ytC_yTYxddQhfg"
+  },
+  {
+    artist: "Ugo Rondinone",
+    gallery: "Sadie Coles HQ",
+    image: "https://images.seesawmap.com/unsafe/filters:quality(95):format(jpeg)/https%3A//seesaw-photos.s3.amazonaws.com/35C9vbwlkyHzzJatqfGkOA"
   }
-  // Add more!
 ];
-
